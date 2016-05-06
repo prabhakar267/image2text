d7c5b8c759b14a72a0d47daac9c8cf6f6888072b
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-05-06 14:09:03 +0530

    pytesser added

---
+++ pytesser/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: prabhakar
+# @Date:   2016-05-06 14:01:36
+# @Last Modified by:   prabhakar
+# @Last Modified time: 2016-05-06 14:01:36
--- pytesser/main.py
+++ pytesser/main.py
@@ -2,10 +2,11 @@
 # @Author: prabhakar
 # @Date:   2016-05-03 20:05:15
 # @Last Modified by:   Prabhakar Gupta
-# @Last Modified time: 2016-05-06 14:08:49
+# @Last Modified time: 2016-05-03 20:16:35
+
 
 from PIL import Image
-from pytesser.pytesser import *
+from pytesser import *
 
 image_file = 'asd/Screenshot from 2016-05-03 20:15:50.png'
 im = Image.open(image_file)
