fb6aaef8e21e9f886911fc47547ef575ab8ce976
Yajana <yajana.merahkee@gmail.com>
2018-10-04 20:24:46 +0530

    tests are added

---
+++ tests/test_core.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import unittest
+from unittest.mock import MagicMock
+from main import main
+import builtins
+import mock
+
+
+def side_effect(value):
+    return value
+
+# os.makedirs = MagicMock(return True)
+class TestCore(unittest.TestCase):
+    def test_setup(self):
+        from mock import call
+        with mock.patch('builtins.print') as mock_print:
+            output = main('sample','output')
+            mock_print.assert_has_calls(
+            [
+                call("tesseract-ocr missing, use sudo apt-get install tesseract-ocr to resolve")
+            ]
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
