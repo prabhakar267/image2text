e4d764fb1e4a083d7129078701370b8731d95a8d
Aditya AVS <avs.aditya@gmail.com>
2018-10-20 13:14:04 +0530

    Added function to check pre-requisites and checks for Windows

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 DEFAULT_OUTPUT_DIRECTORY_NAME = "converted-text"
+
 DEFAULT_CHECK_COMMAND = "which"
 WINDOWS_CHECK_COMMAND = "where"
+TESSERACT_DATA_PATH_VAR = 'TESSDATA_PREFIX'
 
 VALID_IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".gif", ".png", ".tga", ".tif", ".bmp"]
