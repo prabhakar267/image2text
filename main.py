875942aa7e81f7094dee64dfe5566931190d9c89
Adelin Bacanu <adelin.bacanu@gmail.com>
2018-10-21 19:01:44 +0300

    Fix #29 Add single file input argument

--- main.py
+++ main.py
@@ -37,6 +37,15 @@ def get_command():
     return DEFAULT_CHECK_COMMAND
 
 
+def run_tesseract(filename, output_path, image_file_name):
+    # Run tesseract
+    filename_without_extension = os.path.splitext(filename)[0]
+    text_file_path = os.path.join(output_path, filename_without_extension)
+    subprocess.run(['tesseract', image_file_name, text_file_path],
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+
+
 def check_pre_requisites_tesseract():
     """
     Check if the pre-requisites required for running the tesseract application are satisfied or not
@@ -44,16 +53,20 @@ def check_pre_requisites_tesseract():
     :return: boolean
     """
     check_command = get_command()
-    logging.debug("Running `{}` to check if tesseract is installed or not.".format(check_command))
-    result = subprocess.run([check_command, 'tesseract'], stdout=subprocess.PIPE)
+    logging.debug(
+        "Running `{}` to check if tesseract is installed or not.".format(check_command))
+    result = subprocess.run(
+        [check_command, 'tesseract'], stdout=subprocess.PIPE)
     if not result.stdout:
-        logging.error("tesseract-ocr missing, use install `tesseract` to resolve.")
+        logging.error(
+            "tesseract-ocr missing, use install `tesseract` to resolve.")
         return False
     logging.debug("Tesseract correctly installed!\n")
 
     if sys.platform.startswith('win'):
         environment_variables = os.environ
-        logging.debug("Checking if the Tesseract Data path is set correctly or not.\n")
+        logging.debug(
+            "Checking if the Tesseract Data path is set correctly or not.\n")
         if TESSERACT_DATA_PATH_VAR in environment_variables:
             if environment_variables[TESSERACT_DATA_PATH_VAR]:
                 path = environment_variables[TESSERACT_DATA_PATH_VAR]
@@ -63,7 +76,8 @@ def check_pre_requisites_tesseract():
                     logging.debug("All set to go!")
                     return True
                 else:
-                    logging.error("Configured path for Tesseract data is not accessible!")
+                    logging.error(
+                        "Configured path for Tesseract data is not accessible!")
                     return False
             else:
                 logging.error("Tesseract Data path Environment variable '{}' configured to an empty string!\
@@ -84,63 +98,79 @@ def main(input_path, output_path):
 
     # Check if a valid input directory is given or not
     if not check_path(input_path):
-        logging.error("No directory found at `{}`".format(input_path))
+        logging.error("Nothing found at `{}`".format(input_path))
         return
-
-    # Check if input directory is empty or not
-    total_file_count = len(os.listdir(input_path))
-    if total_file_count == 0:
-        logging.error("No files found at your input location")
-        return
-
     # Create output directory
     create_directory(output_path)
 
-    # Iterate over all images in the input directory
-    # and get text from each image
-    other_files = 0
-    successful_files = 0
-    logging.info("Found total {} file(s)\n".format(total_file_count))
-    for ctr, filename in enumerate(os.listdir(input_path)):
-        logging.debug("Parsing {}".format(filename))
-        extension = os.path.splitext(filename)[1]
+    # Check if input_path is directory or file
+    if os.path.isdir(input_path):
 
-        if extension.lower() not in VALID_IMAGE_EXTENSIONS:
-            other_files += 1
-            continue
+        # Check if input directory is empty or not
+        total_file_count = len(os.listdir(input_path))
+        if total_file_count == 0:
+            logging.error("No files found at your input location")
+            return
 
-        image_file_name = os.path.join(input_path, filename)
-        filename_without_extension = os.path.splitext(filename)[0]
-        text_file_path = os.path.join(output_path, filename_without_extension)
-        subprocess.run(['tesseract', image_file_name, text_file_path],
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-        logging.debug("Successfully parsed {}".format(filename))
-        successful_files += 1
+        # Iterate over all images in the input directory
+        # and get text from each image
+        other_files = 0
+        successful_files = 0
+        logging.info("Found total {} file(s)\n".format(total_file_count))
+        for ctr, filename in enumerate(os.listdir(input_path)):
+            logging.debug("Parsing {}".format(filename))
+            extension = os.path.splitext(filename)[1]
 
-    logging.info("Parsing Completed!\n")
-    if successful_files == 0:
-        logging.error("No valid image file found.")
-        logging.error("Supported formats: [{}]".format(", ".join(VALID_IMAGE_EXTENSIONS)))
+            if extension.lower() not in VALID_IMAGE_EXTENSIONS:
+                other_files += 1
+                continue
+
+            image_file_name = os.path.join(input_path, filename)
+            run_tesseract(filename, output_path, image_file_name)
+            successful_files += 1
+
+        logging.info("Parsing Completed!\n")
+        if successful_files == 0:
+            logging.error("No valid image file found.")
+            logging.error("Supported formats: [{}]".format(
+                ", ".join(VALID_IMAGE_EXTENSIONS)))
+        else:
+            logging.info(
+                "Successfully parsed images: {}".format(successful_files))
+            logging.info(
+                "Files with unsupported file extensions: {}".format(other_files))
+
     else:
-        logging.info("Successfully parsed images: {}".format(successful_files))
-        logging.info("Files with unsupported file extensions: {}".format(other_files))
+        filename = os.path.basename(input_path)
+        run_tesseract(filename, output_path, filename)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', help="Input directory where input images are stored", required=True)
+    parser.add_argument(
+        '--input_dir', help="Input directory where input images are stored")
+    parser.add_argument('--input_file', help="Input image filepath")
     parser.add_argument('--output_dir', nargs='?',
                         help="(Optional) Output directory for converted text (default: {input_path}/converted-text)")
     parser.add_argument('--debug', action='store_true',
                         help="Enable verbose DEBUG logging")
     args = parser.parse_args()
-
-    input_path = os.path.abspath(args.input_dir)
+    if not args.input_dir and not args.input_file:
+        parser.error('Required either --input_file or --input_dir')
+    if args.input_dir:
+        input_path = os.path.abspath(args.input_dir)
+    else:
+        input_path = os.path.abspath(args.input_file)
     if args.output_dir:
         output_path = os.path.abspath(args.output_dir)
     else:
-        output_path = os.path.join(input_path, DEFAULT_OUTPUT_DIRECTORY_NAME)
+        if os.path.isdir(input_path):
+            output_path = os.path.join(
+                input_path, DEFAULT_OUTPUT_DIRECTORY_NAME)
+        else:
+            dir_path = os.path.dirname(input_path)
+            output_path = os.path.join(dir_path, DEFAULT_OUTPUT_DIRECTORY_NAME)
+
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
     else:
